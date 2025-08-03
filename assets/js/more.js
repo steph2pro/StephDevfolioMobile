590c0e930a2a0da53c4d46a77ae0762aa631ec94
steph2pro <stephane2004kamga@gmail.com>
2025-08-03 21:46:07 +0100

    nouvelle update

--- assets/js/more.js
+++ assets/js/more.js
@@ -1,10 +1,23 @@
-document.getElementById("toggle-btn").addEventListener("click", function() {
-    var techList = document.getElementById("tech-list");
-    if (techList.classList.contains("hidden")) {
-        techList.classList.remove("hidden");
-        this.textContent = "Voir moins";
-    } else {
-        techList.classList.add("hidden");
-        this.textContent = "Voir plus";
-    }
+document.querySelectorAll(".toggle-btn").forEach(function(button) {
+    button.addEventListener("click", function() {
+        const techList = this.closest(".skills__container").querySelector(".tech-list");
+        
+        if (!techList) return; // sécurité
+
+        techList.classList.toggle("hidden");
+        this.textContent = techList.classList.contains("hidden") ? "Voir plus" : "Voir moins";
+    });
 });
+
+
+
+// document.getElementById("toggle-btn").addEventListener("click", function() {
+//     var techList = document.getElementById("tech-list");
+//     if (techList.classList.contains("hidden")) {
+//         techList.classList.remove("hidden");
+//         this.textContent = "Voir moins";
+//     } else {
+//         techList.classList.add("hidden");
+//         this.textContent = "Voir plus";
+//     }
+// });
